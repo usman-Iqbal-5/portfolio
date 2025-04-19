bd317ff713fc4893c7e4f63c0b8f75363ca57bc2
usman-Iqbal-5 <usmaniqbal912@outlook.com>
2025-04-19 23:09:58 +0100

    Further animation

---
+++ docs/js/all-pages.js
@@ -0,0 +1,6 @@
+AOS.init({
+    offset: 200, // Start animation when the section is 200px from the viewport
+    duration: 600, // Animation duration
+    easing: 'ease-out', // Easing function
+    delay: 100, // Delay before animation starts
+  });
