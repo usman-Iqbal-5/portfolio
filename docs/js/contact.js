6ae120866cb06bab27ffe05d5cc7518c6f43923b
usman-Iqbal-5 <usmaniqbal912@outlook.com>
2025-06-03 17:33:45 +0100

    added favicon and edited navbar

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -1,17 +1,17 @@
 // Handle form submission
 document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();  // Prevent the form from submitting the traditional way
+  e.preventDefault();  
 
   // Submit the form to Google Forms using iframe
   var iframe = document.getElementById('hidden_iframe');
   this.target = 'hidden_iframe';
 
   iframe.onload = function () {
-    // Once submission is successful, reset the form and show thank you message
-    document.getElementById('contactForm').reset(); // Reset the form fields
-    document.getElementById('thankYouMessage').style.display = 'block'; // Show thank you message
+  
+    document.getElementById('contactForm').reset(); 
+    document.getElementById('thankYouMessage').style.display = 'block';
     setTimeout(function () {
-      document.getElementById('thankYouMessage').style.display = 'none'; // Hide message after 5 seconds
+      document.getElementById('thankYouMessage').style.display = 'none'; 
     }, 5000);
   };
 
