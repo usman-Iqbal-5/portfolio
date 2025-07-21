82d903c99290b6feb89552c02f367e530059d097
usman-Iqbal-5 <usmaniqbal912@outlook.com>
2025-07-21 17:35:30 +0100

    Updated the footer - dynamic date loading

--- docs/js/all-pages.js
+++ docs/js/all-pages.js
@@ -1,8 +1,12 @@
-AOS.init({
-    offset: 200, // Start animation when the section is 200px from the viewport
-    duration: 600, // Animation duration
-    easing: 'ease-out', // Easing function
-    delay: 100, // Delay before animation starts
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const date = new Date().getFullYear();
+  const dateContainer = document.querySelector(".date");
+  dateContainer.innerHTML = date;
+});
 
-  
+AOS.init({
+  offset: 200, // Start animation when the section is 200px from the viewport
+  duration: 600, // Animation duration
+  easing: "ease-out", // Easing function
+  delay: 100, // Delay before animation starts
+});
